18abf051899d8f47aca72144e4cc69b86b81d0c4
jitu gandhare <jitugandhare@gmail.com>
2023-10-19 11:22:45 +0530

    ok

---
+++ myapp/src/App.js
@@ -0,0 +1,30 @@
+import logo from "./logo.svg";
+import "./App.css";
+import AllRoutes from "./AllRoutes/AllRoutes";
+import Navbar from "./component/Navbar";
+import Home from "./Pages/Home";
+import Skills from "./Pages/Skills";
+import { Box } from "@chakra-ui/react";
+import About from "./Pages/About";
+import Projects from "./Pages/Projects";
+import Contact from "./Pages/Contact";
+
+function App() {
+  return (
+    <Box
+      className="App"
+      fontFamily={"cursive"}
+      backgroundColor={"rgb(29, 28, 28)"}
+    >
+      <Navbar />
+      {/* <AllRoutes /> */}
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </Box>
+  );
+}
+
+export default App;
